91593fac7f255f4498c072413bb7c98bcc42ad56
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-04-02 14:39:08 -0300

    [ADD] clv_processing_log_jcafb

---
+++ clv_processing_log_jcafb/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Processing Log (for CLVhealth-JCAFB Solution)',
+    'summary': 'Processing Log Module used in CLVhealth-JCAFB Solution.',
+    'version': '15.0.6.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'images': [],
+    'depends': [
+        'clv_processing',
+        'clv_global_log',
+    ],
+    'data': [
+        'views/processing_host_log_view.xml',
+        'views/processing_template_log_view.xml',
+        'views/processing_schedule_log_view.xml',
+        'data/global_log_client.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
---
+++ clv_processing_log_jcafb/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import processing_host_log
+from . import processing_template_log
+from . import processing_schedule_log
+# # from . import processing_batch_log
